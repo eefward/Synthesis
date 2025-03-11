a208422ab07ea96c8a40304ddb3cc9529ea6c9af
Guest <guestythebesty841@gmail.com>
2025-03-11 10:53:53 -0400

    incomplete menu

---
+++ static/scripts/menuChanges.js
@@ -0,0 +1,16 @@
+function toggleInfoBox(isVisible, debounce) {
+    const infoBox = document.getElementById('information');
+    
+    if (isVisible) {
+        infoBox.style.opacity = '0';
+        setTimeout(() => {
+            infoBox.style.display = 'none';
+        }, debounce);
+    } else {
+        infoBox.style.display = 'block';
+        setTimeout(() => {
+            infoBox.style.opacity = '1';
+        }, 10);
+    }
+}
+
--- static/scripts/visualNotes.js
+++ static/scripts/visualNotes.js
@@ -1,19 +1,3 @@
-function toggleInfoBox(isVisible, debounce) {
-    const infoBox = document.getElementById('information');
-    
-    if (isVisible) {
-        infoBox.style.opacity = '0';
-        setTimeout(() => {
-            infoBox.style.display = 'none';
-        }, debounce);
-    } else {
-        infoBox.style.display = 'block';
-        setTimeout(() => {
-            infoBox.style.opacity = '1';
-        }, 10);
-    }
-}
-
 function createSlideEffect(key) {
     const bar = document.createElement('div');
     bar.classList.add('slide-bar');
