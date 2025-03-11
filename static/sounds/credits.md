4d85a8d4d9ff9f785e88be5a7ad2fccb9b2366f2
GuestyTheBesty <guestythebesty841@gmail.com>
2025-03-10 20:53:05 -0400

    that was stupd

--- static/sounds/credits.md
+++ static/sounds/credits.md
@@ -1,3 +1,6 @@
 https://github.com/daviddeborin/Piano-Sounds-High-Quality
 
-what a nice guy
+such a nice man
+
+I wrote a python script to automate changing 1.mp3 to A0, 88.mp3 to C8, etc.  
+That python script gave me cancer
---
+++ static/sounds/rename_to_midi_format.py
@@ -0,0 +1,22 @@
+import os
+
+dir = "fullpath"
+notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
+for file in os.scandir(dir):
+    a = file.path.split("\\")[-1]
+    # if you're using linux do .split("/")
+
+    b = a.split(".")
+    i = int(b[0]) - 1
+
+    note = notes[i % 12]
+    octave = (i + 9) // 12
+    
+    c = f"{note}{octave}" + ".mp3"
+
+    old = file.path
+    new = os.path.join(dir, c)
+    os.rename(old, new)
+    
+    
