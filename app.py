ec929b216eb641994b2821704f75cfa1d2f3da14
GuestyTheBesty <guestythebesty841@gmail.com>
2025-03-11 00:14:40 -0400

    idk if i tmatters but prob noit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, redirect, render_template, request, jsonify, session
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/")
 def home():
