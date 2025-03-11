4bf977d3d3b6a58aa9bf67428239d06959f2889f
GuestyTheBesty <guestythebesty841@gmail.com>
2025-03-10 21:03:24 -0400

    im himmity

--- static/playSounds.js
+++ static/playSounds.js
@@ -4,5 +4,9 @@ const information = document.getElementById('information');
 keys.forEach(key => {
     key.addEventListener('click', () => {
         information.innerText = `You pressed ${key.dataset.note}`;
+        
+        // the # messes with the url so you have to use that function (makes it # become %23)
+        const audio = new Audio(`/static/sounds/${encodeURIComponent(key.dataset.note)}.mp3`)
+        audio.play()
     });
 });
