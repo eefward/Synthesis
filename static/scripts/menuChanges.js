4c773b59090db1f84c5d3aefe9c5cd46a0944701
GuestyTheBesty <guestythebesty841@gmail.com>
2025-03-17 17:16:26 -0400

    almost htere

--- static/scripts/menuChanges.js
+++ static/scripts/menuChanges.js
@@ -26,31 +26,48 @@ keyToggleButton.addEventListener('click', () => {
 
 // --------------------------------------------------
 
+const css = document.getElementById('slidingBarsCSS');
+
 function validateInput() {
     const redTop = document.getElementById('redTop');
     const greenTop = document.getElementById('greenTop');
     const blueTop = document.getElementById('blueTop');
     const transparencyTop = document.getElementById('transparencyTop');
     const redBottom = document.getElementById('redBottom');
-    const greenBottom= document.getElementById('greenBottom');
+    const greenBottom = document.getElementById('greenBottom');
     const blueBottom = document.getElementById('blueBottom');
     const transparencyBotom = document.getElementById('tranparencyBottom');
 
     const colors = [redTop, greenTop, blueTop, redBottom, greenBottom, blueBottom];
     for (let i = 0; i < colors.length; i++) {
-        const parsed = parseInt(colors[i].value);
-        if (!(Number.isInteger(parsed) && parsed.toString() === colors[i].trim())) {
-            return alert("Integers and valid input only");
-        } else if (!(0 <= parsed && parsed <= 255)) {
-            return alert("Color values can only be 0-255");
-        }
+        const num = Number(colors[i].value);
+
+        if (isNaN(num)) return alert("Integers and valid input only");
+        else if (!(0 <= num && num <= 255)) return alert("Color values can only be 0-255");
     }
 
+    const transparencies = [transparencyTop, transparencyBotom];
+    for (let i = 0; i < transparencies.length; i++) {
+        const num = Number(transparencies[i].value);
 
+        if (isNaN(num)) return alert("Integers and valid input only");
+        else if (!(0 <= num && num <= 1)) return alert("Transparency values can only be 0-1");
+    }
+
+    css.innerHTML = `
+        .whiteSlidingBar {
+            background: linear-gradient(135deg, rgba(255, 99, 71, 0.9) 0%, rgba(255, 0, 0, 1) 100%); /* Tomato to Red */
+            transition: transform 1s ease-out, opacity 1s ease-out;
+        }
+        .blackSlidingBar {
+            background: linear-gradient(135deg, rgba(255, 99, 71, 0.9) 0%, rgba(139, 0, 0, 1) 100%); /* Dark Red */
+            transition: transform 1s ease-out, opacity 1s ease-out;
+        }
+    `;
 }
 
 const selectedColor = document.getElementById('selectColor');
-const css = document.getElementById('slidingBarsCSS');
+
 
 selectedColor.addEventListener('change', () => {
     const color = selectedColor.value;
