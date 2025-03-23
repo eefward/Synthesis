fe8226420fe3fa8cd07b353fc86aa42c8d1cc70a
eefward <edwardy347@gmail.com>
2025-03-23 12:15:58 -0400

    falling effect

--- static/scripts/functions.js
+++ static/scripts/functions.js
@@ -53,6 +53,53 @@ function playNoteWithEffect(note) {
     }, disappear);
 }
 
+function playNoteWithReversedEffect(note, fallDuration) {
+    const element = document.querySelector(`[data-note="${note}"]`);
+
+    // properties
+    const whitestartPos = '-40vh'; 
+    const blackstartPos = '-35vh'
+    const fadeOutTime = 600;
+
+    const bar = document.createElement('div');
+    document.getElementById('slidingBars').appendChild(bar);
+    bar.classList.add('slide-bar');
+
+    const keyRect = element.getBoundingClientRect();
+
+    if (element.classList.contains('white-key')) {
+        bar.classList.add('whiteSlidingBar');
+        bar.style.top = whitestartPos;
+        bar.style.width = `${keyRect.width * 0.9}px`; 
+        bar.style.height = `${keyRect.height * 1.25}px`; 
+    } else if (element.classList.contains('black-key')) {
+        bar.classList.add('blackSlidingBar');
+        bar.style.top = blackstartPos;
+        bar.style.width = `${keyRect.width}px`; 
+        bar.style.height = `${keyRect.height * 1.25}px`;
+    }
+
+    bar.style.left = `${keyRect.left + keyRect.width / 2 - parseFloat(bar.style.width) / 2}px`;
+
+    bar.style.transition = `transform ${fallDuration}ms linear`;
+    requestAnimationFrame(() => {
+        bar.style.transform = 'translateY(100vh)';
+    });
+
+    setTimeout(() => {
+        const audio = new Audio(`/static/sounds/${encodeURIComponent(note)}.mp3`);
+        audio.currentTime = 0;
+        audio.play();
+        bar.style.opacity = '0';
+        keyClickEffect(element)
+    }, fallDuration);
+
+    setTimeout(() => {
+        bar.remove();
+    }, fallDuration + fadeOutTime);
+}
+
+
 function keyClickEffect(key) {
     const darkenAmt = "brightness(70%)";
     key.style.filter = darkenAmt; 
@@ -209,16 +256,21 @@ function playRecording() {
     }
 
     let lastNoteTime = recordedNotes[recordedNotes.length - 1].time; 
-    let totalDuration = lastNoteTime * delayBeforePlaying; 
-
+    
     recordedNotes.forEach(noteData => {
-        const delay = noteData.time * delayBeforePlaying;
+        let delay = noteData.time * delayBeforePlaying; 
+
         setTimeout(() => {
-            playNoteWithEffect(noteData.note);
+            playNoteWithReversedEffect(noteData.note, delayBeforePlaying);
         }, delay);
     });
 
+    let totalDuration = lastNoteTime * delayBeforePlaying;
     setTimeout(() => {
         playButton.textContent = 'Play Recording';
     }, totalDuration);
 }
+
+
+
+
