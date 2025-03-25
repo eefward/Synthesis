ed2466cb5bf8af35bee66b07e95fddf6b95b410e
Guest <guestythebesty841@gmail.com>
2025-03-25 09:27:48 -0400

    made creating the pinao within the js rather than html

--- static/scripts/keyPressed.js
+++ static/scripts/keyPressed.js
@@ -4,7 +4,6 @@ let isMouseDown = false; // Track if mouse is pressed
 let lastPlayedNote = null; // Track the last played note to avoid repetition
 let lastPlayedTime = 0; // Store the time the last note was played
 const cooldown = 100; // Cooldown in milliseconds (e.g., 100ms = 0.1s)
-
 let recordingStartTime = null;
 let isRecording = false;
 let recordedNotes = [];
