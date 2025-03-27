0fe6a4e075ac9585fdb6c2fd3f1ba79b8ea6c90b
eefward <edwardy347@gmail.com>
2025-03-27 12:17:27 -0400

    reverssed gl

--- static/scripts/keyPressed.js
+++ static/scripts/keyPressed.js
@@ -5,19 +5,8 @@ let isRecording = false;
 const ttlVal = Number(document.getElementById('ttl').value);
 const ttl = isNaN(ttlVal) || ttlVal === 0 ? 1000 : ttlVal;
 
-let recordingStartTime = null;
-let recordedNotes = [];
-
 keys.forEach(key => {
     key.addEventListener('mousedown', () => {
-        if (recordingStartTime == null) {
-            recordingStartTime = Date.now();
-        }
-
-        const elapsedTime = (Date.now() - recordingStartTime) / 1000; 
-        recordedNotes.push({ note: key.dataset.note, time: elapsedTime }); 
         playNote(key, ttl, isRecording);
-
-        console.log(recordedNotes);
     });
 });
