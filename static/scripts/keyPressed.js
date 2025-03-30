89ff0c1316b1d4936617e10a4434633f8cd7585a
GuestyTheBesty <guestythebesty841@gmail.com>
2025-03-30 01:03:37 -0400

    renamed to duration cuz ttl (time to live) is kinda for packets

--- static/scripts/keyPressed.js
+++ static/scripts/keyPressed.js
@@ -3,11 +3,11 @@ const information = document.getElementById('notePressedInfo');
 
 keys.forEach(key => {
     key.addEventListener('mousedown', () => {
-        const ttlVal = Number(document.getElementById('duration').value);
-        const ttl = isNaN(ttlVal) || ttlVal === 0 ? 5000 : ttlVal;
+        let duration = Number(document.getElementById('duration').value);
+        duration = isNaN(duration) || duration === 0 ? 5000 : duration;
 
-        if (isRecording) recording.push({note: key.dataset.note, time: Date.now() - recording[0].time, duration: ttl});
-        playNote(key.dataset.note, ttl);
+        if (isRecording) recording.push({note: key.dataset.note, time: Date.now() - recording[0].time, duration: duration});
+        playNote(key.dataset.note, duration);
         createNoteAnimation(key);
         pressedDownFX(key, 150); // second parameter is duration, good for key holding
     });
