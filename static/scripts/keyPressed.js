864ef16f5640bed0fb16ec7cf11d90f098cc5b66
GuestyTheBesty <guestythebesty841@gmail.com>
2025-03-30 00:59:43 -0400

    duration wroks

--- static/scripts/keyPressed.js
+++ static/scripts/keyPressed.js
@@ -1,13 +1,13 @@
 const keys = document.querySelectorAll('.key');
 const information = document.getElementById('notePressedInfo');
 
-const ttlVal = Number(document.getElementById('ttl').value);
-const ttl = isNaN(ttlVal) || ttlVal === 0 ? 1000 : ttlVal;
-
 keys.forEach(key => {
     key.addEventListener('mousedown', () => {
-        if (isRecording) recording.push({note: key.dataset.note, time: Date.now() - recording[0].time, duration: 5});
-        playNote(key.dataset.note, 5000);
+        const ttlVal = Number(document.getElementById('duration').value);
+        const ttl = isNaN(ttlVal) || ttlVal === 0 ? 5000 : ttlVal;
+
+        if (isRecording) recording.push({note: key.dataset.note, time: Date.now() - recording[0].time, duration: ttl});
+        playNote(key.dataset.note, ttl);
         createNoteAnimation(key);
     });
 });
